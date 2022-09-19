ed590215ef6affb3384cba1e97a2e2179cfc3714
Somraj Mukherjee <iamsomraj@gmail.com>
2022-09-19 14:22:54 +0530

    👌 IMPROVE: added color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,20 @@ module.exports = {
         sans: ['glacial-regular', 'ui-sans-serif', 'system-ui'],
         'glacial-bold': ['glacial-bold', 'ui-sans-serif', 'system-ui'],
       },
+      colors: {
+        dark: {
+          DEFAULT: '#171716',
+        },
+        ash: {
+          DEFAULT: '#323232',
+        },
+        mint: {
+          DEFAULT: '#DFEEEE',
+        },
+        skin: {
+          DEFAULT: '#FBF9ED',
+        },
+      },
     },
   },
   plugins: [],
