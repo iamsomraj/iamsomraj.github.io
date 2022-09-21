7a21974849adc04307ce8edae3a1674bf06f85c4
Somraj Mukherjee <iamsomraj@gmail.com>
2022-09-21 20:48:08 +0530

    📦 NEW: added background blobs to default layout

--- components/user-interface/background-blob.vue
+++ components/user-interface/background-blob.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="z-20">
     <div
-      class="fixed top-1/2 left-1/2 h-56 w-56 animate-blob rounded-full bg-muddy opacity-70 mix-blend-darken filter blur-xl dark:bg-muddy dark:mix-blend-overlay"
+      class="my-blur fixed top-1/2 left-1/2 h-56 w-56 animate-blob rounded-full bg-muddy opacity-70 mix-blend-darken filter dark:bg-muddy dark:mix-blend-overlay"
     ></div>
     <div
-      class="animation-delay-2000 fixed top-1/2 right-1/2 h-56 w-56 animate-blob rounded-full bg-yellowish opacity-70 mix-blend-darken filter blur-xl dark:bg-yellowish dark:mix-blend-overlay"
+      class="animation-delay-2000 my-blur fixed top-1/2 right-1/2 h-56 w-56 animate-blob rounded-full bg-yellowish opacity-70 mix-blend-darken filter dark:bg-yellowish dark:mix-blend-overlay"
     ></div>
     <div
-      class="animation-delay-4000 fixed bottom-1/3 left-1/3 h-56 w-56 animate-blob rounded-full bg-orangy opacity-70 mix-blend-darken filter blur-xl dark:bg-orangy dark:mix-blend-overlay"
+      class="animation-delay-4000 my-blur fixed bottom-1/3 left-1/3 h-56 w-56 animate-blob rounded-full bg-orangy opacity-70 mix-blend-darken filter dark:bg-orangy dark:mix-blend-overlay"
     ></div>
   </div>
 </template>
@@ -18,3 +18,9 @@ export default {
   name: 'BackgroundBlob',
 }
 </script>
+
+<style scoped>
+.my-blur {
+  filter: blur(24px);
+}
+</style>
