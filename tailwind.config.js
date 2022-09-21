b1516e31699c40f9330d3332a50a845c62d35a4e
Somraj Mukherjee <iamsomraj@gmail.com>
2022-09-21 19:00:07 +0530

    📦 NEW: added new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,39 @@ module.exports = {
   ],
   theme: {
     extend: {
+      animation: {
+        blob: 'blob 7s infinite',
+      },
+      keyframes: {
+        blob: {
+          '0%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+          '33%': {
+            transform: 'translate(30px, -70px) scale(1.1)',
+          },
+          '66%': {
+            transform: 'translate(-30px, 20px) scale(0.9)',
+          },
+          '100%': {
+            transform: 'tranlate(0px, 0px) scale(1)',
+          },
+        },
+      },
       fontFamily: {
         sans: ['glacial-regular', 'ui-sans-serif', 'system-ui'],
         'glacial-bold': ['glacial-bold', 'ui-sans-serif', 'system-ui'],
       },
       colors: {
+        muddy: {
+          DEFAULT: '#DF8876',
+        },
+        orangy: {
+          DEFAULT: '#FFBA6A',
+        },
+        yellowish: {
+          DEFAULT: '#F9F871',
+        },
         dark: {
           DEFAULT: '#171716',
         },
