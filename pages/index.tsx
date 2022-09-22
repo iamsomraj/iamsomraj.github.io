eac244949a6cce9b358f755a2aedc444d79393d2
Somraj Mukherjee <iamsomraj@gmail.com>
2022-09-23 01:50:30 +0530

    📦 NEW: next js added with ts, eslint, prettier

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <h1 className="container mx-auto px-4 py-2 text-center text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
+};
+
+export default Home;
