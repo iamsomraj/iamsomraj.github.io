12398219be97fb2720539264c91c608e835dc060
Somraj Mukherjee <iamsomraj@gmail.com>
2022-10-09 16:32:21 +0530

    📦 NEW: sanity added

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
   const title = `Somraj's Portfolio`;
 
   return (
-    <div className="z z-0 h-screen snap-y snap-mandatory overflow-scroll overflow-x-hidden overflow-y-scroll bg-[rgb(36,36,36)] text-white scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-primary-orange/80">
+    <div className="z-0 h-screen snap-y snap-mandatory overflow-scroll overflow-x-hidden overflow-y-scroll bg-[rgb(36,36,36)] text-white scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-primary-orange/80">
       <Head>
         <title>{title}</title>
       </Head>
