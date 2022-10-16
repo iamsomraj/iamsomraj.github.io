172dddeb8073da3371f125a077bfbd3fe8bd5cbd
Somraj Mukherjee <iamsomraj@gmail.com>
2022-10-17 01:16:23 +0530

    👌 IMPROVE: project section design updated

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,7 +1,6 @@
-import Image from 'next/image';
 import { motion } from 'framer-motion';
-import { IProject } from '../type';
 import { urlFor } from '../sanity';
+import { IProject } from '../type';
 
 type Props = {
   projects: IProject[];
@@ -13,36 +12,27 @@ const Projects = ({ projects }: Props) => {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="relative z-0 mx-auto flex h-screen max-w-full flex-col items-center justify-evenly overflow-hidden text-left md:flex-row"
+      className="relative flex h-screen max-w-full flex-col items-center justify-center overflow-hidden"
     >
-      {/* Begin: Section Title */}
-      <h3 className="absolute top-24 text-2xl uppercase tracking-[20px] text-gray-500">
-        Projects
-      </h3>
-      {/* End: Section Title */}
-
-      <div className="relative z-20 flex w-full snap-x snap-mandatory overflow-y-hidden overflow-x-scroll scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-primary-orange/80">
+      <div className="relative z-20 flex w-full snap-x snap-mandatory overflow-y-hidden overflow-x-scroll scrollbar scrollbar-track-mint-green/80 scrollbar-thumb-dark-gray/80">
         {projects.map((project, index) => (
           <div
             key={project._id}
-            className="flex h-screen w-screen flex-shrink-0 snap-center flex-col items-center justify-center space-y-5 p-20"
+            className="flex h-screen w-screen flex-shrink-0 snap-center flex-col items-center justify-center space-y-2 px-10"
           >
             <motion.div
               initial={{ opacity: 0 }}
               transition={{ duration: 1.2 }}
               whileInView={{ opacity: 1 }}
               viewport={{ once: true }}
-              className="flex items-center justify-center overflow-hidden break-words rounded-lg px-4 py-6 text-left text-5xl lowercase tracking-widest text-white  underline decoration-primary-orange/50"
+              className="flex items-center justify-center overflow-hidden break-words rounded-lg px-4 py-6 text-left text-5xl lowercase tracking-widest text-dark-gray"
             >
               {project.title}
             </motion.div>
 
             <div className="max-w-6xl space-y-10 px-0 md:px-10">
-              <h4>
-                <span className="mr-2 underline decoration-primary-orange/50">
-                  Case Study {index + 1} of {projects.length}:
-                </span>
-                {project.title}
+              <h4 className="mr-2 text-center font-bold text-dark-gray">
+                Project {index + 1} of {projects.length}
               </h4>
 
               <div className="flex space-x-2">
@@ -52,7 +42,7 @@ const Projects = ({ projects }: Props) => {
                     tech.image && (
                       <motion.img
                         key={tech._id}
-                        className="h-10 w-10 flex-shrink-0 rounded-full object-cover object-center"
+                        className="h-10 w-10 flex-shrink-0 rounded-full border border-dark-gray/60 object-cover object-center"
                         src={urlFor(tech.image).url()}
                         alt={tech.title}
                       />
@@ -60,15 +50,21 @@ const Projects = ({ projects }: Props) => {
                 )}
               </div>
 
-              <p className="whitespace-pre-line text-center text-base leading-relaxed md:text-left">
+              <motion.div
+                initial={{ opacity: 0 }}
+                transition={{ duration: 1.2 }}
+                whileInView={{ opacity: 1 }}
+                viewport={{ once: true }}
+                className="text-center text-base leading-relaxed text-dark-gray"
+              >
                 {project.summary}
-              </p>
+              </motion.div>
             </div>
           </div>
         ))}
       </div>
 
-      <div className="absolute top-[30%] left-0 h-[500px] w-full -skew-y-12 bg-primary-orange/10"></div>
+      <div className="absolute top-[30%] left-0 h-[500px] w-full -skew-y-12 bg-mint-green"></div>
     </motion.div>
   );
 };
