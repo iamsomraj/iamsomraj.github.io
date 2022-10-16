0c6625eee84c966686ad77b581e09401d25719ca
Somraj Mukherjee <iamsomraj@gmail.com>
2022-10-16 15:32:30 +0530

    👌 IMPROVE: fixed about section

--- components/About.tsx
+++ components/About.tsx
@@ -12,46 +12,57 @@ const About = ({ pageInfo }: Props) => {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="relative mx-auto flex h-screen max-w-7xl flex-col items-center justify-center space-y-5 px-10 text-center md:flex-row md:space-y-0 md:space-x-5 md:text-left"
+      className="flex h-screen flex-col items-center justify-center space-y-8"
     >
       {/* Begin: Section Title */}
-      <h3 className="absolute top-24 text-2xl uppercase tracking-[20px] text-gray-500">
+      <h3 className="text-2xl uppercase tracking-[20px] text-gray-500">
         About
       </h3>
       {/* End: Section Title */}
 
-      {/* Begin: About Page Image */}
-      <div className="flex w-full items-center justify-center">
-        {pageInfo?.profilePicture && (
-          <motion.img
-            initial={{ x: -200, opacity: 0 }}
-            whileInView={{ x: 0, opacity: 1 }}
-            transition={{ duration: 1.2 }}
-            viewport={{ once: true }}
-            src={urlFor(pageInfo?.profilePicture).url()}
-            alt={pageInfo.name}
-            className="h-32 w-32 flex-shrink-0 rounded-full object-cover md:h-48 md:w-48 md:rounded-lg"
-          />
-        )}
-      </div>
-      {/* End: About Page Image */}
+      {/* Begin: About Page Elements */}
+      <div className="flex flex-col items-center justify-start space-y-2 md:flex-row md:space-x-8 md:space-y-0">
+        {/* Begin: About Page Image */}
+        <div className="flex w-full items-center justify-center">
+          {pageInfo?.profilePicture && (
+            <motion.img
+              initial={{ x: -200, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1.2 }}
+              viewport={{ once: true }}
+              src={urlFor(pageInfo?.profilePicture).url()}
+              alt={pageInfo.name}
+              className="h-32 w-32 flex-shrink-0 rounded-full object-cover md:h-48 md:w-48 md:rounded-lg"
+            />
+          )}
+        </div>
+        {/* End: About Page Image */}
 
-      <motion.div
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        transition={{ duration: 1.5 }}
-        viewport={{ once: true }}
-        className="flex w-full flex-col space-y-2"
-      >
-        <h4 className="text-3xl font-semibold">
-          Here is a{' '}
-          <span className="underline decoration-primary-orange/50">little</span>{' '}
-          background
-        </h4>
-        <p className="text-sm leading-loose">
-          {pageInfo?.backgroundInformation}
-        </p>
-      </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 1.2 }}
+          viewport={{ once: true }}
+          className="flex max-w-lg flex-col space-y-2 px-10"
+        >
+          <h4 className="text-center text-3xl font-semibold md:text-left">
+            Here is a{' '}
+            <span className="underline decoration-primary-orange/50">
+              little
+            </span>{' '}
+            background
+          </h4>
+          <motion.p
+            initial={{ opacity: 0.5 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            className="break-words text-center text-sm leading-loose md:text-left"
+          >
+            {pageInfo?.backgroundInformation}
+          </motion.p>
+        </motion.div>
+      </div>
+      {/* End: About Page Elements */}
     </motion.div>
   );
 };
