ef5915b0adf2ee0b2500800c0b144817488e1849
Somraj Mukherjee <iamsomraj@gmail.com>
2022-10-16 11:35:33 +0530

    👌 IMPROVE: header design updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,61 +9,64 @@ type Props = {
 
 const Header = ({ socials }: Props) => {
   return (
-    <header className="sticky top-0 z-20 mx-auto flex max-w-7xl flex-row items-start justify-between p-5">
-      {/* Begin: Left Section */}
-      <motion.div
-        initial={{
-          x: -500,
-          opacity: 0.5,
-          scale: 0.5,
-        }}
-        animate={{ x: 0, opacity: 1, scale: 1 }}
-        transition={{ duration: 1.5 }}
-        className="flex flex-row items-center"
-      >
-        {socials.map((social) => (
-          <SocialIcon
-            key={social._id}
-            url={social.url}
-            fgColor="gray"
-            bgColor="transparent"
-          />
-        ))}
-      </motion.div>
-      {/* End: Left Section */}
-
-      {/* Begin: Right Section */}
-      <Link href="#contact">
+    <header className="sticky top-0 z-20 w-full shadow-sm">
+      <div className="mx-auto flex max-w-7xl flex-row items-start justify-between p-5">
+        {/* Begin: Left Section */}
         <motion.div
           initial={{
-            x: 500,
+            x: -500,
             opacity: 0.5,
             scale: 0.5,
           }}
-          animate={{
-            x: 0,
-            opacity: 1,
-            scale: 1,
-          }}
-          transition={{
-            duration: 1.5,
-          }}
-          className="flex cursor-pointer flex-row items-center text-gray-300"
+          animate={{ x: 0, opacity: 1, scale: 1 }}
+          transition={{ duration: 1.5 }}
+          className="flex flex-row items-center"
         >
-          {/* Begin: Mail Icon */}
-          <SocialIcon
-            className="cursor-pointer"
-            network="email"
-            fgColor="gray"
-            bgColor="transparent"
-          />
-          {/* End: Mail Icon */}
-          <p className="hidden text-sm uppercase text-gray-400 md:inline-flex">
-            Get in Touch
-          </p>
+          {socials.map((social) => (
+            <SocialIcon
+              key={social._id}
+              url={social.url}
+              fgColor="gray"
+              bgColor="transparent"
+            />
+          ))}
         </motion.div>
-      </Link>
-      {/* End: Right Section */}
+        {/* End: Left Section */}
+
+        {/* Todo: Add Theme Toggle Button */}
+        {/* Begin: Right Section */}
+        <Link href="#contact">
+          <motion.div
+            initial={{
+              x: 500,
+              opacity: 0.5,
+              scale: 0.5,
+            }}
+            animate={{
+              x: 0,
+              opacity: 1,
+              scale: 1,
+            }}
+            transition={{
+              duration: 1.5,
+            }}
+            className="flex cursor-pointer flex-row items-center text-gray-300"
+          >
+            {/* Begin: Mail Icon */}
+            <SocialIcon
+              className="cursor-pointer"
+              network="email"
+              fgColor="gray"
+              bgColor="transparent"
+            />
+            {/* End: Mail Icon */}
+            <p className="hidden text-sm uppercase text-gray-400 md:inline-flex">
+              Get in Touch
+            </p>
+          </motion.div>
+        </Link>
+        {/* End: Right Section */}
+      </div>
     </header>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ const Home = ({ pageInfo, experiences, projects, skills, socials }: Props) => {
   const title = `Somraj's Portfolio`;
 
   return (
-    <div className="z-0 h-screen snap-y snap-mandatory overflow-scroll overflow-x-hidden overflow-y-scroll scroll-smooth bg-[rgb(36,36,36)] text-white scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-primary-orange/80">
+    <div className="h-screen overflow-scroll overflow-x-hidden overflow-y-scroll scroll-smooth bg-off-white text-dark-gray scrollbar scrollbar-track-mint-green/80 scrollbar-thumb-dark-gray/80">
       <Head>
         <title>{title}</title>
       </Head>
